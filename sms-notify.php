c74d7f07fde0851f3b5b0ca6cbaea8deb52eda2e
Alex Swan <alex@bold-it.com>
2017-06-23 20:26:40 -0500

    initial

---
+++ sms-notify.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @package SMS_Notify
+ * @version 1.0
+ */
+/*
+Plugin Name: SMS Notify
+Plugin URI: http://wordpress.org/plugins/sms_notify/
+Description: This has two functions. First, it adds a 'phone' field to users' profiles, along with a 'notifications' check box. Second, it adds a hook to send an SMS to each user when a post is published.
+Author: Alex Swan
+Version: 1.0
+Author URI: http://bold-it.com/
+*/
+
+?>
